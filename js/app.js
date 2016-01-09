310e777f961db1f158cacd486e3d755d2b5aa858
Claudia Forgione <claudia4gione@gmail.com>
2016-01-09 16:47:53 -0430

    Mi primer commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(e){
+	var name = 'claudia'
+	console.log(name)
+
+	var result = 5 + 9
+
+	console.log(result)
+	alert('Gelow')
+
+	var cabecera = document.getElementById('cabecera').innerHTML
+
+	console.log(cabecera)
+})
